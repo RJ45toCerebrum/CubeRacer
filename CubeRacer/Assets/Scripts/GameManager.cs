7f4bbcbe2f66221fc99891a8e845264383b2fcb3
Tyler Heers <tyler.heers@gmail.com>
2019-01-26 01:27:34 -0800

    SideMovement
    SideMovement

--- CubeRacer/Assets/Scripts/GameManager.cs
+++ CubeRacer/Assets/Scripts/GameManager.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Vector3 gameSpeed;
+    public static Lane entryLane;
+    public Lane player1Start, player2Start;
+    public PlayerController player1, player2;
+
+    public GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Debug.LogError("Not alloed to have multiple game managers....");
+
+        // setup player one
+        player1.transform.position = player1Start.transform.position;
+        player1.transform.rotation = player1Start.transform.rotation;
+        player1.CurrentLane = player1Start;
+    }
+
+
 }
--- CubeRacer/Assets/Scripts/PlayerController.cs
+++ CubeRacer/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,22 @@ public class PlayerController : MonoBehaviour
 {
     public Vector3 playerSpeed;
     private Rigidbody playerRB;
+    private Lane currentLane;
+    public float laneSwitchTime = 0.3f;
+
+    // movement time
+    private Lane targetLane;
+    private Vector2 movementDirection;
+    private float sideMovementTime;
+
+
+    private bool isMoving = false;
+
+    public Lane CurrentLane
+    {
+        get { return currentLane; }
+        set { currentLane = value; }
+    }
 
     private void Awake() {
         playerRB = GetComponent<Rigidbody>();
@@ -14,6 +30,54 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        playerRB.MovePosition(playerRB.position + playerSpeed * Time.fixedDeltaTime);
+        Vector3 PF = Vector3.zero;
+        if (isMoving)
+        {
+            if (!isClose())
+            {
+                Vector2 S = currentLane.transform.position;
+                Vector3 w = (S + (sideMovementTime / laneSwitchTime) * movementDirection);
+                PF += w;
+                sideMovementTime += Time.deltaTime;
+                PF.z += transform.position.z + playerSpeed.z * Time.deltaTime;
+                transform.position = PF;
+            }
+            else
+            {
+                isMoving = false;
+                sideMovementTime = 0;
+                transform.position = transform.position + playerSpeed * Time.fixedDeltaTime;
+                currentLane = targetLane;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && !isMoving)
+            StartMoveLeft();
+        else if (Input.GetKeyDown(KeyCode.D) && !isMoving)
+            StartMoveRight();
+        else
+            transform.position = transform.position + playerSpeed * Time.fixedDeltaTime;
+    }
+
+    private void StartMoveLeft()
+    {
+        isMoving = true;
+        targetLane = currentLane.leftLane;
+        movementDirection = targetLane.transform.position - currentLane.transform.position;
+        sideMovementTime = 0;
+    }
+
+    private void StartMoveRight()
+    {
+        isMoving = true;
+        targetLane = currentLane.rightLane;
+        movementDirection = targetLane.transform.position - currentLane.transform.position;
+        sideMovementTime = 0;
+    }
+
+    private bool isClose() {
+        Vector2 s = transform.position;
+        Vector2 f = targetLane.transform.position;
+        float sqrd = (s - f).sqrMagnitude;
+        return sqrd < 0.01f;
     }
 }
