d3bf2a215af45a1f7c9406b0436634a10eb493cc
Ben Loesch <benloesch@gmail.com>
2019-01-26 01:34:44 -0800

    Finished double jump

---
+++ CubeRacer/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour {
+
+    public float jumpHeight = 1f;
+    public float movespeed = 1f;
+    public float jumpTime = 1f;
+    public int jumpcount;
+
+    private Vector3 speed;
+    private Vector3 jump;
+
+    // Use this for initialization
+    void Start () {
+        speed = new Vector3(0, 0, movespeed);
+        jump = new Vector3(0, jumpHeight, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Moves player forward
+        transform.position += speed * Time.deltaTime;
+
+        if(jumpcount == 1)
+        {
+            if(jumpTime >= 0f)
+            {
+                jumpTime -= Time.deltaTime;
+            }
+            else
+            {
+                //drop
+                jumpcount--;
+                transform.position -= jump;
+                Debug.Log("Plop!");
+
+            }
+        }else if(jumpcount == 2)
+        {
+            if (jumpTime >= 0f)
+            {
+                jumpTime -= Time.deltaTime;
+            }
+            else
+            {
+                //drop
+                jumpcount--;
+                transform.position -= jump;
+                Debug.Log("Plop!");
+                jumpTime = 1f;
+            }
+        }
+        else if(jumpcount > 2)
+        {
+            Debug.Log("Too many jumps!");
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.W)))
+        {
+            transform.position += jump;
+            jumpcount++;
+            jumpTime = 1;
+        }
+       
+    }
+
+}
