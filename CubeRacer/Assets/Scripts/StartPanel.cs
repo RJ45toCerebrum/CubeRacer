1b8640d970ca3889174ae8a5f04c2a37f88f5ac1
Ben Loesch <benloesch@gmail.com>
2019-01-27 19:33:17 -0800

    Added more obstacle collisions to the level. Also added a few more UI elements.

---
+++ CubeRacer/Assets/Scripts/StartPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPanel : MonoBehaviour {
+
+    public GameObject startPanel;
+
+    void Awake()
+    {
+        startPanel.SetActive(true);
+        Time.timeScale = 0f; //Game paused
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClosePanel()
+    {
+        startPanel.SetActive(false);
+        Time.timeScale = 1f; //Unpause
+    }
+}
