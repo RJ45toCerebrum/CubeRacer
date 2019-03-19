4b6764a1bafa31c384d1d458f5144c8738adb6c0
Ben <benloesch@gmail.com>
2019-03-18 22:19:41 -0700

    Completed framework for Proc Gen. This is as far as I'm taking it for the demo. I'm also adding Susannah's dope environment art models (though I still need to add textures)

---
+++ CubeRacer/Assets/Scripts/ProceduralLevelGen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralLevelGen : MonoBehaviour {
+
+    public int numSections;     //This dictates the number of level sections spawned/level
+    public Object[] level_sections;
+    public float zSizeLevel;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Debug.Log("Z-Length of obj: " + obj.transform.localScale.z);
+
+        //Finds sections of level based on their tag, then pools them into level_sections
+        level_sections = Resources.LoadAll("Prefabs/LevelSectionsHard", typeof(GameObject));
+
+        //GameObject go = (GameObject)level_sections[0];
+        //Debug.Log("Z " + go.transform.localScale.z);
+
+        //foreach (var v in level_sections)
+        //{
+        //    Debug.Log(v.name);
+        //}
+
+        OrganizeLevel();
+    }
+
+    //Places level sections in a [random] order
+    void OrganizeLevel()
+    {
+        for(int i = 0; i < numSections; i++)
+        {
+            int r = Random.Range(0, level_sections.Length);
+            Debug.Log("Random number " + r);
+
+            Instantiate(level_sections[r], new Vector3(0, 0, zSizeLevel), Quaternion.identity);
+            //zSizeLevel += level_sections[r].transform.localScale.z;
+            zSizeLevel += 200;
+        }
+    }
+}
---
+++ CubeRacer/Assets/Scripts/RandomGeneration.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGeneration : MonoBehaviour {
+
+    /*This script generates a random placement of obstacles on an N x 0 x 5 grid [X x Y x Z]
+     After obstacle is placed, increment X value by 10*/
+
+    public GameObject obstacle;
+    public GameObject blank;
+
+    public int sizeX;
+    public int difficulty;
+
+    // Use this for initialization
+    void Start () {
+        //GameObject[,] lvl = new GameObject[sizeX, 3];
+        //InitGridGen(lvl);
+        RandomGen();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //void InitGridGen(GameObject[,] lvl)
+    //{
+    //    for (int z = 0; z < sizeX; z++)      //first we start at X pos = 0
+    //    {
+    //        for (int x = 0; x < 3; x++)     //Here we instantiate an object for each Y pos at the current X pos
+    //        {
+    //            GameObject go = Instantiate(blank, new Vector3(x * 10f, 0, z * 10f), Quaternion.identity);
+    //            go.transform.SetParent(this.transform, false);
+    //            lvl[z,x] = go;
+    //        }
+    //    }
+    //    //Display(lvl);
+    //}
+
+    //void Display(GameObject[,] lvl)
+    //{
+    //    for(int i = 0; i < sizeX; i++)
+    //    {
+    //        for(int j = 0; j < 3; j++)
+    //        {
+    //            Debug.Log("obstacle at X: " + lvl[i,j].transform.position.x + " and Z: " + lvl[i, j].transform.position.z);
+    //        }
+    //    }
+    //}
+
+    void RandomGen()
+    {
+        for(int z = 0; z < sizeX; z++)
+        {
+            int r = Random.Range(1, 100);
+            Debug.Log("1st Random number chosen: " + r);
+            if(r < difficulty)  //First random check
+            {
+                int r2 = Random.Range(0, 3); //decides number of obstacles to spawn at this Z pos
+                Debug.Log("2nd Random number chosen: " + r2);
+
+                for (int i = 0; i < r2; i++)
+                {
+                    int r3 = Random.Range(0, 3); //decides where to place obstacles
+                    Debug.Log("2nd Random number chosen: " + r3);
+
+                    for (int x = 0; x < 3; x++)  //Second random check
+                    {
+                        if (r3 == x)
+                        {
+                            GameObject go = Instantiate(obstacle, new Vector3(x * 10f, 0, z * 10f), Quaternion.identity);
+                            go.transform.SetParent(this.transform, false);
+                            //lvl[z, x] = go;
+                        }
+                        else
+                        {
+                            GameObject go = Instantiate(blank, new Vector3(x * 10f, 0, z * 10f), Quaternion.identity);
+                            go.transform.SetParent(this.transform, false);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
